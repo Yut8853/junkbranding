7a9a4a8f136b5fa962acd0186f3c1bc3c1fc5214
Yutaka Kizaki <factory0611@gmail.com>
2026-04-18 18:40:13 +0900

    sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -23,6 +23,12 @@ export default function sitemap(): MetadataRoute.Sitemap {
       changeFrequency: 'weekly',
       priority: 0.9,
     },
+        {
+      url: `${baseUrl}/pricing`,
+      lastModified: new Date(),
+      changeFrequency: 'weekly',
+      priority: 0.8,
+    },
     {
       url: `${baseUrl}/contact`,
       lastModified: new Date(),
